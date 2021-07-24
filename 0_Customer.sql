f5b8ceb83cdfd2c7cdefc95da1ab84835f5940c2
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-07-24 23:54:41 +0800

    Creating Employee Table

--- 0_Customer.sql
+++ 0_Customer.sql
@@ -1,22 +1,21 @@
 DROP TABLE Customer;
 CREATE TABLE Customer
-(CustomerID     VARCHAR(5)   NOT NULL,
- CustomerName   VARCHAR(50)  NOT NULL,
- Gender         VARCHAR(1)   NOT NULL,
- ContactNo      VARCHAR(15)  NOT NULL,
+(CustomerID     VARCHAR2(5)   NOT NULL,
+ CustomerName   VARCHAR2(50)  NOT NULL,
+ Gender         VARCHAR2(1)   NOT NULL,
+ ContactNo      VARCHAR2(15)  NOT NULL,
  DOB            DATE, 
- Email          VARCHAR(50)  NOT NULL,
- StreetName     VARCHAR(100) NOT NULL,
+ Email          VARCHAR2(50)  NOT NULL,
+ StreetName     VARCHAR2(100) NOT NULL,
  PostCode       NUMBER(7)    NOT NULL,
- City           VARCHAR(30)  NOT NULL,
- State          VARCHAR(30)  NOT NULL,
-PRIMARY KEY(CustomerID),
-CONSTRAINT chk_cust_id CHECK (SUBSTR(CustomerID, 1, 1) = 'C'),
-CONSTRAINT ContactNo UNIQUE (ContactNo),
-CONSTRAINT chk_cust_gender CHECK (UPPER(Gender) IN ('M','F')),
-CONSTRAINT chk_email CHECK (REGEXP_LIKE(email,'^[a-zA-Z]\w+@(\S+)$'))
+ City           VARCHAR2(30)  NOT NULL,
+ State          VARCHAR2(30)  NOT NULL,
+ PRIMARY KEY(CustomerID),
+ CONSTRAINT chk_cust_id CHECK (SUBSTR(CustomerID, 0, 1) = 'C'),
+ CONSTRAINT chk_ContactNo UNIQUE (ContactNo),
+ CONSTRAINT chk_cust_gender CHECK (UPPER(Gender) IN ('M','F')),
+ CONSTRAINT chk_email CHECK (REGEXP_LIKE(email,'^[a-zA-Z]\w+@(\S+)$'))
 );
-
 
 
 -- 200 records
