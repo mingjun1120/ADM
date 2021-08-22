86baec22d2d4b897561522828fed3a1c2dedfa7e
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-08-23 01:35:30 +0800

    Update Trigger.sql

--- Lim Chia Chung/Trigger.sql
+++ Lim Chia Chung/Trigger.sql
@@ -5,7 +5,7 @@
 -- operating time which is between 10am to 4pm. The appointment will no be updated if the time
 -- is out of operating hours.
 
-DROP TRIGGER TRG_VALIDATE_OPERATING_HR;
+-- DROP TRIGGER TRG_VALIDATE_OPERATING_HR;
 CREATE OR REPLACE TRIGGER TRG_VALIDATE_OPERATING_HR
 BEFORE INSERT OR UPDATE OF StartTime, EndTime ON Appointment
 FOR EACH ROW
@@ -15,12 +15,18 @@ DECLARE
 BEGIN
    -- v_startTime := :NEW.StartTime;
    -- v_endTime := :NEW.EndTime;
-   CASE WHEN UPDATING THEN
-      IF :NEW.StartTime < 10 OR TO_NUMBER(TO_CHAR(:NEW.EndTime, 'HH24')) > 19 THEN
-         RAISE_APPLICATION_ERROR(-20000, 'Not Operating Hour');
+   IF INSERTING OR UPDATING THEN
+      IF :NEW.StartTime = :OLD.StartTime THEN
+         RAISE_APPLICATION_ERROR(-20000, 'No Changes Made');
+      ELSIF :NEW.StartTime < 10 OR TO_NUMBER(TO_CHAR(:NEW.EndTime, 'HH24')) > 19 THEN
+         RAISE_APPLICATION_ERROR(-20001, 'Not Operating Hour');
       END IF;
-   END CASE;
+   -- ELSIF INSERTING THEN
+            
+   END IF;
 END;
 /
+
+INSERT INTO Appointment VALUES ('A10081', 'C1010', 'SER001', 'PET004', 'E001', TO_DATE('30-06-2018', 'DD-MM-YYYY'), '09:00', '10:00', 1);
 
 ---------------------------------------- Trigger 2 ---------------------------------------------
