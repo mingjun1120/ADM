7b2857c4e2ccc7db1f94fb7af4cba5ee6530c092
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-08-23 22:10:15 +0800

    Update Procedure.sql

--- Lim Chia Chung/Procedure.sql
+++ Lim Chia Chung/Procedure.sql
@@ -1,13 +1,13 @@
 ---------------------------------------- Procedure 1 ---------------------------------------------
 
--- Update The Appointment Time
+-- Update The Appointment Date And Time
 -- Purpose: Purpose: The purpose of this procedure is used to update the appointment time. Once
 -- the start time has been set, the end time will also be updated based on the
 
 ALTER SESSION SET NLS_DATE_FORMAT='DD-MM-YYYY';
 ALTER SESSION SET NLS_TIMESTAMP_FORMAT='HH24:MI';
 
-CREATE OR REPLACE PROCEDURE PRC_UPDATE_APPOINTMENT_TIME(in_appointmentID VARCHAR,
+CREATE OR REPLACE PROCEDURE PRC_UPDATE_APPOINTMENT_DATE_TIME(in_appointmentID VARCHAR,
                                                         in_appointmentDate DATE,
                                                         in_startTime TIMESTAMP) IS
    NO_APPOINTMENT EXCEPTION;
@@ -35,8 +35,6 @@ BEGIN
       v_EndTime := in_startTime + appointment_rec.Duration / 24;
       DBMS_OUTPUT.PUT_LINE(v_EndTime);
 
-      -- IF in_startTime < 10 OR TO_NUMBER(TO_CHAR(v_EndTime, 'HH24')) > 16 THEN
-      --    RAISE INVALID_OPERATING_TIME;
       UPDATE Appointment
       SET    AppointmentDate = in_appointmentDate, StartTime = in_startTime, EndTime = v_EndTime
       WHERE  AppointmentID = in_appointmentID;
@@ -68,17 +66,13 @@ BEGIN
          DBMS_OUTPUT.PUT_LINE('+NOT OPERATING DAY+');
          DBMS_OUTPUT.PUT_LINE('+++++++++++++++++++');
          DBMS_OUTPUT.PUT_LINE('Operating days is from Monday to Friday');
-      WHEN OTHERS THEN
-         DBMS_OUTPUT.PUT_LINE('+++++++++++++++++');
-         DBMS_OUTPUT.PUT_LINE('+NO CHANGES MADE+');
-         DBMS_OUTPUT.PUT_LINE('+++++++++++++++++');
 END;
 /
 
 SET SERVEROUTPUT ON
-EXEC PRC_UPDATE_APPOINTMENT_TIME('A10079', TO_DATE('20-08-2021', 'DD-MM-YYYY'), TO_TIMESTAMP('12:00', 'HH24:MI'));
-EXEC PRC_UPDATE_APPOINTMENT_TIME('A10080', TO_TIMESTAMP('20:00', 'HH24:MI'));
-EXEC PRC_UPDATE_APPOINTMENT_TIME('A10081', TO_TIMESTAMP('09:00', 'HH24:MI'));
+EXEC PRC_UPDATE_APPOINTMENT_DATE_TIME('A10079', TO_DATE('20-08-2021', 'DD-MM-YYYY'), TO_TIMESTAMP('12:00', 'HH24:MI'));
+EXEC PRC_UPDATE_APPOINTMENT_DATE_TIME('A10080', TO_TIMESTAMP('20:00', 'HH24:MI'));
+EXEC PRC_UPDATE_APPOINTMENT_DATE_TIME('A10081', TO_TIMESTAMP('09:00', 'HH24:MI'));
 
 SET LINESIZE 120
 SET PAGESIZE 140
@@ -100,12 +94,46 @@ WHERE AppointmentID = 'A10080' OR AppointmentID = 'A10079';
 
 ---------------------------------------- Procedure 2 ---------------------------------------------
 
--- 
+-- Create Appointment
 -- Purpose: Purpose: The purpose of this procedure is to
 
-CREATE OF REPLACE PROCEDURE
+CREATE OF REPLACE PROCEDURE PRC_CREATE_APPOINTMENT IS
+   -- NO_APPOINTMENT EXCEPTION;
+   -- INVALID_OPERATING_TIME EXCEPTION;
+   -- PRAGMA EXCEPTION_INIT(INVALID_OPERATING_TIME, -20001);
+   -- INVALID_WEEKDAY EXCEPTION;
+   -- PRAGMA EXCEPTION_INIT(INVALID_WEEKDAY, -20002);
+ 
+   v_duration Appointment.Duration%TYPE;
+   v_EndTime Appointment.EndTime%TYPE;
+   
+   CURSOR APP_CURSOR IS
+      SELECT *
+      FROM   Appointment
+      WHERE  AppointmentID = in_appointmentID;
+   
+   CURSOR CUST_CURSOR IS
+      SELECT *
+      FROM   Customer
+      WHERE  AppointmentID = in_appointmentID;
 
+   appointment_rec APP_CURSOR%ROWTYPE;  
 BEGIN
 
 END;
 /
+
+ACCEPT custName CHAR FORMAT A50-
+PROMPT 'Enter Customer Name: '
+
+ACCEPT appDate DATE FORMAT 'DD-MM-YYYY'
+PROMPT 'Enter Appointment Date (DD-MM-YYYY): '
+
+ACCEPT serviceName CHAR FORMAT A50-
+PROMPT 'Enter Service Name: '
+
+ACCEPT petName CHAR FORMAT A50-
+PROMPT 'Enter Pet Name: '
+
+EXEC PRC_CREATE_APPOINTMENT();
+INSERT INTO Appointment VALUES ('A'||APPOINTMENT_SEQ.NEXTVAL, 'C1010', 'SER001', 'PET004', 'E001', TO_DATE('30-07-2018', 'DD-MM-YYYY'), '12:00', '14:00', 2);
