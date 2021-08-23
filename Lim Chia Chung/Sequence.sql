23d085a4cb1dd86448f416112e66d65513d8d403
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-08-23 23:55:42 +0800

    Done

--- Lim Chia Chung/Sequence.sql
+++ Lim Chia Chung/Sequence.sql
@@ -11,3 +11,18 @@ CREATE SEQUENCE APPOINTMENT_SEQ
                 NOCYCLE;
 
 -- INSERT INTO Appointment VALUES ('A'||APPOINTMENT_SEQ.NEXTVAL, 'C1010', 'SER001', 'PET004', 'E001', TO_DATE('30-07-2018', 'DD-MM-YYYY'), '12:00', '14:00', 2);
+
+
+---------------------------------------- Sequence 2 ---------------------------------------------
+
+-- Pet Sequence
+
+DROP SEQUENCE PET_SEQ;
+CREATE SEQUENCE PET_SEQ
+                INCREMENT BY 1
+                START WITH 1021
+                MAXVALUE 99999999
+                NOCACHE
+                NOCYCLE;
+
+INSERT INTO Pet VALUES ('PET'||PET_SEQ.NEXTVAL, 'LCC', 'Ragdoll', 'M', 'White');
