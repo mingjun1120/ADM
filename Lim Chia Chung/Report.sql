08c7bf24f78bacdd3e1b832a4b509a3414e5607f
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-08-26 11:21:14 +0800

    Update Report.sql

--- Lim Chia Chung/Report.sql
+++ Lim Chia Chung/Report.sql
@@ -6,12 +6,37 @@
 -- of the business. In addition, the objective of the service performance report is to identify 
 -- which services are worth investing un and which ones considered whether to keep this service
 
+SET LINESIZE 1200
+SET PAGESIZE 600
+CLEAR COLUMNS
+CLEAR BREAKS
+CLEAR COMPUTES
+CLEAR BUFFER
+TTITLE OFF
+
 CREATE OR REPLACE PROCEDURE PRC_SERVICE_PERFORMANCE_REPORT IS
+   CURSOR SER_CURSOR IS
+      SELECT *
+      FROM   Services;
 
+    CURSOR APP_CURSOR (v_serviceName VARCHAR) IS
+       SELECT *
+       FROM   Appointment
+       WHERE  ServiceName = v_serviceName 
+
+    
 BEGIN
+   DBMS_OUTPUT.PUT_LINE('Service Performance Report');
+   DBMS_OUTPUT.PUT_LINE('From 01-JAN-2018 to 30-Jun-2018');
 
+   FOR service_rec IN SER_CURSOR LOOP
+   
+   END LOOP;
 END;
 /
+
+SET SERVEROUTPUT ON
+EXEC PRC_SERVICE_PERFORMANCE_REPORT;
 
 ---------------------------------------- Report 2 ---------------------------------------------
 
