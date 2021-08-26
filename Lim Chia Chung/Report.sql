79f3df1510f4999accf232dbd395f83c00647d30
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-08-26 15:59:06 +0800

    Update Report.sql

--- Lim Chia Chung/Report.sql
+++ Lim Chia Chung/Report.sql
@@ -19,19 +19,31 @@ CREATE OR REPLACE PROCEDURE PRC_SERVICE_PERFORMANCE_REPORT IS
       SELECT *
       FROM   Services;
 
-    CURSOR APP_CURSOR (v_serviceName VARCHAR) IS
-       SELECT *
-       FROM   Appointment
-       WHERE  ServiceName = v_serviceName 
-
-    
+   --  CURSOR APP_CURSOR (v_serviceName VARCHAR) IS
+   --     SELECT *
+   --     FROM   Appointment
+   --     WHERE  ServiceName = v_serviceName;
+   -- appointment_rec APP_CURSOR%ROWTYPE;
 BEGIN
+   -- OPEN APP_CURSOR;
+   -- FETCH APP_CURSOR INTO appointment_rec;
+   FOR v_counter IN 1 .. 5 LOOP
+      DBMS_OUTPUT.PUT_LINE(CHR(10));
+   END LOOP;
+
    DBMS_OUTPUT.PUT_LINE('Service Performance Report');
    DBMS_OUTPUT.PUT_LINE('From 01-JAN-2018 to 30-Jun-2018');
 
-   FOR service_rec IN SER_CURSOR LOOP
-   
+   FOR services_rec IN SER_CURSOR LOOP
+      DBMS_OUTPUT.PUT_LINE(CHR(10));
+      DBMS_OUTPUT.PUT_LINE('Service Name: ' || services_rec.ServiceName);
+      DBMS_OUTPUT.PUT_LINE(RPAD('-',150,'-'));
+      DBMS_OUTPUT.PUT_LINE(RPAD('Date', 15,' ') || RPAD('Time', 15,' ') || RPAD('Appointment', 15,' ') ||
+                           RPAD('Customer', 20,' ') || RPAD('Pet', 15,' ')  || RPAD('Service Charges (RM)', 22,' ') ||
+                           );
    END LOOP;
+
+   -- CLOSE APP_CURSOR;
 END;
 /
 
